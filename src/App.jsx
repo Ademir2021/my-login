87fd76e1804ab4a476a66a51c8f2a0d4aeb4a14b
Ademir Souza de Almeida <centroserra@gmail.com>
2022-06-30 18:53:10 -0300

    Subindo aplicação

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import AppRoutes from "./AppRoutes";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppRoutes/>
+      </div>
+    );
+  }
+}
+
+export default App;
